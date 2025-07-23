b502349ed35a430967370b879a844a01030942ce
JuneYoung <nietsnie1@gmail.com>
2025-07-23 10:47:25 +0900

    feat: Improve column name consistency across the module

--- universal_timeseries_transformer/timeseries_application.py
+++ universal_timeseries_transformer/timeseries_application.py
@@ -25,7 +25,13 @@ def transform_timeseries_to_cumreturns(timeseries):
     return df_cumreturns
 
 def transform_timeseries_to_cumreturns_ref_by_series(timeseries, srs_ref):
-    cumreturns_ref = (timeseries / srs_ref -1) * 100
+    cumreturns_ref = (timeseries / srs_ref - 1) * 100
+    cumreturns_ref.columns = [f"cumreturn: {col}" for col in cumreturns_ref.columns]
+    return cumreturns_ref
+
+def transform_timeseries_to_cumreturns_ref_by_series_ref_date(timeseries, date_ref):
+    srs_row_ref = timeseries.loc[date_ref]
+    cumreturns_ref = transform_timeseries_to_cumreturns_ref_by_series(timeseries, srs_row_ref)
     return cumreturns_ref
 
 def transform_timeseries_to_cumreturns_ref_by_index(timeseries, index_ref):
